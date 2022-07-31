7be4d0279d71deb3754299667c33699d512168af
Ethansnow2012 <ethansnow2012@gmail.com>
2022-07-31 16:50:04 +0800

    feat: dirty playground

---
+++ src/stories/DirtyPlayground.stories.ts
@@ -0,0 +1,33 @@
+
+import GParent from '../components/GParent.vue';
+import GChildren from '../components/GChildren.vue';
+import { Meta, Story } from '@storybook/vue3'
+import {ref} from 'vue'
+
+export default {
+  title: 'My dirty playGround',
+  component: [GParent, GChildren],
+  argTypes: {
+  },
+};
+
+const Template: Story  = (args ) => ({
+  components: { GParent, GChildren },
+  setup() {
+    const textFromRoot = ref({str: 'textFromRoot'})
+    return { args, textFromRoot, handleCustomChange:(payload)=>{ console.log('ssssss',payload)} }
+  },
+  template: `
+    <div>
+      <GParent  >
+        <GChildren :text1="textFromRoot" @someevent="(a,payload)=>{handleCustomChange(payload)}"/>
+      </GParent>
+    </div>
+    `,
+})
+
+
+export const Primary = Template.bind({});
+Primary.args = {}
+
+
